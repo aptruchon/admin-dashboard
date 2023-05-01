ae8a2551c851fdfabe1b5f65a0efb737bdd459e2
Alex P. Truchon <aptruchon@gmail.com>
2023-05-01 15:36:08 -0400

    Created header component and products scene

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,12 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
-  console.log(mode);
-  
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-  console.log(theme);
-  
   return (
     <div className="app">
       <BrowserRouter>
@@ -24,6 +21,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
