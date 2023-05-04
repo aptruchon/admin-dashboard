3b31204f600d4cd3044cb105f3eee7b74f421e1d
Alex P. Truchon <aptruchon@gmail.com>
2023-05-04 16:26:35 -0400

    Formatted data to match line chart expected input

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -7,6 +7,41 @@ const OverviewChart = ({ isDashboard = false, view }) => {
   const theme = useTheme();
   const { data, isLoading } = useGetSalesQuery();
 
+  const [totalSalesLine, totalUnitsLine] = useMemo(() => {
+    if (!data) return [];
+
+    const { monthlyData } = data;
+    const totalSalesLine = {
+      id: "totalSales",
+      color: theme.palette.secondary.main,
+      data: [],
+    };
+    const totalUnitsLine = {
+      id: "totalUnits",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+
+    Object.values(monthlyData).reduce(
+      (acc, { month, totalSales, totalUnits }) => {
+        const curSales = (acc.sales = totalSales);
+        const curUnits = (acc.units = totalUnits);
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x: month, y: curSales },
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x: month, y: curUnits },
+        ];
+
+        return { sale: curSales, units: curUnits };
+      },
+      { sales: 0, units: 0 }
+    );
+  }, [data]);
+
   return <div>OverviewChart</div>;
 };
 
